b9ba973a1579894be4341a5f845d47c552585f76
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2026-04-10 11:37:57 -0300

    criação do exception handler

---
+++ ms-auth/src/main/kotlin/stockflow/com/br/ms_auth/dtos/ErrorDTO.kt
@@ -0,0 +1,11 @@
+package stockflow.com.br.ms_auth.dtos
+
+import java.time.LocalDateTime
+
+data class ErrorDTO(
+    val timestamp: LocalDateTime = LocalDateTime.now(),
+    val status: Int,
+    val error: String,
+    val message: String?,
+    val path: String
+)
---
+++ ms-auth/src/main/kotlin/stockflow/com/br/ms_auth/exceptions/ExceptionHandler.kt
@@ -0,0 +1,92 @@
+package stockflow.com.br.ms_auth.exceptions
+
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import stockflow.com.br.ms_auth.dtos.ErrorDTO
+
+@RestControllerAdvice
+class ExceptionHandler {
+
+    @ExceptionHandler(BadCredentialsException::class)
+    fun handleBadCredentialsException(
+        exception: BadCredentialsException,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorDTO> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            ErrorDTO(
+                status = HttpStatus.BAD_REQUEST.value(),
+                error = HttpStatus.BAD_REQUEST.name,
+                message = exception.message,
+                path = request.servletPath
+            )
+        )
+    }
+
+    @ExceptionHandler(BadRequestException::class)
+    fun handleBadRequestException(
+        exception: BadCredentialsException,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorDTO> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            ErrorDTO(
+                status = HttpStatus.BAD_REQUEST.value(),
+                error = HttpStatus.BAD_REQUEST.name,
+                message = exception.message,
+                path = request.servletPath
+            )
+        )
+    }
+
+    @ExceptionHandler(InvalidTokenException::class)
+    fun handleInvalidTokenException(
+        exception: BadCredentialsException,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorDTO> {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+            ErrorDTO(
+                status = HttpStatus.UNAUTHORIZED.value(),
+                error = HttpStatus.UNAUTHORIZED.name,
+                message = exception.message,
+                path = request.servletPath
+            )
+        )
+    }
+
+    @ExceptionHandler(NotFoundException::class)
+    fun handleNotFoundException(
+        exception: BadCredentialsException,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorDTO> {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+            ErrorDTO(
+                status = HttpStatus.NOT_FOUND.value(),
+                error = HttpStatus.NOT_FOUND.name,
+                message = exception.message,
+                path = request.servletPath
+            )
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationError(
+        exception: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ErrorDTO {
+        val errorMessage = HashMap<String, String?>()
+        exception.bindingResult.fieldErrors.forEach { e ->
+            errorMessage[e.field] = e.defaultMessage
+        }
+        return ErrorDTO(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = errorMessage.toString(),
+            path = request.servletPath
+        )
+    }
+}
