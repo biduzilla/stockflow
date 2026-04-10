2e06778b91f276448a4f499cb841a6c8e76ff02a
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2026-04-10 11:40:35 -0300

    implementação audit

--- ms-auth/src/main/kotlin/stockflow/com/br/ms_auth/MsAuthApplication.kt
+++ ms-auth/src/main/kotlin/stockflow/com/br/ms_auth/MsAuthApplication.kt
@@ -2,8 +2,10 @@ package stockflow.com.br.ms_auth
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 class MsAuthApplication
 
 fun main(args: Array<String>) {
